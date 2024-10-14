bd4d409e583103066753860ad0198e91825fd084
Rutuj Jain <rutujyambal2@gmail.com>
2024-10-14 08:39:55 +0530

    SideBar Added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
-
+import EventImg from '../assets/event.png'
+import CompleteImg from '../assets/calendar-check.png'
+import Dashboard from '../assets/dashboard.png'
+import PendingEvent from '../assets/agenda.png'
+import '../App.css'
 const Sidebar = ({ onCreateEventClick }) => {
     return (
         <div className="sidebar">
@@ -9,11 +12,30 @@ const Sidebar = ({ onCreateEventClick }) => {
             </div>
             <nav>
                 <ul>
-                    <li>Dashboard</li>
-                    <li>Event Category</li>
-                    <li>Create Category</li>
-                    <li>Events</li>
-                    <li onClick={onCreateEventClick}>Create Event</li>
+                    <li>
+                        <div className='sidebar-icons'>
+                            <img src={Dashboard} alt="" />
+                        </div>
+                        <div>Dashboard</div>
+                    </li>
+                    <li>
+                        <div className='sidebar-icons' >
+                            <img src={CompleteImg} alt="" />
+                        </div>
+                        <div>Completed Events</div>
+                    </li>
+                    <li>
+                        <div className='sidebar-icons' style={{ fontWeight: '600' }}>
+                            <img src={PendingEvent} alt="" />
+                        </div>
+                        <div>Incomplete Events</div>
+                    </li>
+                    <li onClick={onCreateEventClick}>
+                        <div className='sidebar-icons' style={{ height: '25px', width: '25px' }}>
+                            <img src={EventImg} alt="" />
+                        </div>
+                        <div>Create Event</div>
+                    </li>
                     {/* Add other menu items */}
                 </ul>
             </nav>
