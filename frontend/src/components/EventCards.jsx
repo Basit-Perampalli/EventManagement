c801a12b85c495a257d46d049f76fd7e900c2d16
Rutuj Jain <rutujyambal2@gmail.com>
2024-10-18 09:15:05 +0530

    Edit Buttons design updated

--- frontend/src/components/EventCards.jsx
+++ frontend/src/components/EventCards.jsx
@@ -3,7 +3,7 @@ import './EventCards.css';
 import '../App.css'
 import { EventContext } from '../context/EventContext';
 
-const EventCards = ({ events,usertype }) => {
+const EventCards = ({ events, usertype }) => {
 
     // const [currentPage, setCurrentPage] = useState(1);
     // const [eventsPerPage] = useState(10);
@@ -21,7 +21,7 @@ const EventCards = ({ events,usertype }) => {
             const token = localStorage.getItem('accessToken')
             const response = await fetch(`http://localhost:8000/event/${eventId}/update/`, {
                 method: 'PUT',
-                headers: { 'Content-Type': 'application/json', 'Authorization':`Bearer ${token}`},
+                headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
                 body: JSON.stringify(selectedEvent)
             });
             if (response.ok) {
@@ -55,78 +55,78 @@ const EventCards = ({ events,usertype }) => {
     return (
         <div className='cards-dashboard'>
 
-            {usertype==='regular'&&
-              <>
-                <div className="search-filters">
-                <div className='innerdiv-eventcard'>
-                    <label htmlFor="titleSearch" className='label-eventcard'>Search by Title</label>
-                    <input
-                        type="text"
-                        id="titleSearch"
-                        className="search-input"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-                </div>
-                <div className='innerdiv-eventcard' >
-                    <label htmlFor="locationFilter" className='label-eventcard'>Search by Location</label>
-                    <input
-                        type="text"
-                        id="locationFilter"
-                        className="search-input"
-                        value={locationFilter}
-                        onChange={(e) => setLocationFilter(e.target.value)}
-                    />
-                </div>
-                <div className='innerdiv-eventcard'>
-                    <label htmlFor="date" className='label-eventcard'>Filter by Date</label>
-                    <input
-                        type="date"
-                        id="date"
-                        className="search-input"
-                        value={dateFilter}
-                        onChange={(e) => setDateFilter(e.target.value)}
-                        />
-                </div>  
-            </div>
-            </>}
+            {usertype === 'regular' &&
+                <>
+                    <div className="search-filters">
+                        <div className='innerdiv-eventcard'>
+                            <label htmlFor="titleSearch" className='label-eventcard'>Search by Title</label>
+                            <input
+                                type="text"
+                                id="titleSearch"
+                                className="search-input"
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                        </div>
+                        <div className='innerdiv-eventcard' >
+                            <label htmlFor="locationFilter" className='label-eventcard'>Search by Location</label>
+                            <input
+                                type="text"
+                                id="locationFilter"
+                                className="search-input"
+                                value={locationFilter}
+                                onChange={(e) => setLocationFilter(e.target.value)}
+                            />
+                        </div>
+                        <div className='innerdiv-eventcard'>
+                            <label htmlFor="date" className='label-eventcard'>Filter by Date</label>
+                            <input
+                                type="date"
+                                id="date"
+                                className="search-input"
+                                value={dateFilter}
+                                onChange={(e) => setDateFilter(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </>}
 
             {/* Event Cards */}
             {/* <h3>Event List</h3> */}
-            {!loading&&
-            <div className="cards-container">
-                {/* <h3>Event List</h3> */}
-                {events.length > 0 ? (
-                    events.map((event, index) => (
-                        <div className="event-card" key={event.id}>
-                            <h3 style={{marginBottom:"20px"}}>{event.title}</h3>
-                            <p style={{marginBottom:"20px"}}>{event.description}</p>
-                            {/* <p><strong>Location:</strong> {event.location}</p> */}
-                            <p><strong>Start Date:</strong> {new Date(event.start_time).toLocaleString()}</p>
-                            <p><strong>End Date:</strong> {new Date(event.end_time).toLocaleString()}</p>
-                            {/* <p><strong>Organizer:</strong> {event.organizer}</p> */}
-                            <p><strong>Type:</strong> {event.is_public? 'Public' : 'Private'}</p>
-                            {
-                                usertype=='organizer'&&
-                                <div className="event-actions">
-                                    <button
-                                        className="action-btn"
-                                        style={{width:"100px"}}
-                                        onClick={() => toggleEventStatus(event.id)}
-                                    >
-                                        {event.is_public ? 'Mark Private' : 'Mark Public'}
-                                    </button>
-                                    <button className="action-btn">View</button>
-                                    <button className="action-btn" onClick={() => handleEditClick(event)}>Edit</button>
-                                    <button className="action-btn delete-btn" onClick={() => deleteEvent(event.id)}>Delete</button>
-                                </div>
-                            }
-                        </div>
-                    ))
-                ) : (
-                    <div className="no-events">No events available</div>
-                )}
-            </div>}
+            {!loading &&
+                <div className="cards-container">
+                    {/* <h3>Event List</h3> */}
+                    {events.length > 0 ? (
+                        events.map((event, index) => (
+                            <div className="event-card" key={event.id}>
+                                <h3 style={{ marginBottom: "20px" }}>{event.title}</h3>
+                                <p style={{ marginBottom: "20px" }}>{event.description}</p>
+                                {/* <p><strong>Location:</strong> {event.location}</p> */}
+                                <p><strong>Start Date:</strong> {new Date(event.start_time).toLocaleString()}</p>
+                                <p><strong>End Date:</strong> {new Date(event.end_time).toLocaleString()}</p>
+                                {/* <p><strong>Organizer:</strong> {event.organizer}</p> */}
+                                <p><strong>Type:</strong> {event.is_public ? 'Public' : 'Private'}</p>
+                                {
+                                    usertype == 'organizer' &&
+                                    <div className="event-actions">
+                                        <button
+                                            className="action-btn"
+                                            style={{ width: "100px" }}
+                                            onClick={() => toggleEventStatus(event.id)}
+                                        >
+                                            {event.is_public ? 'Mark Private' : 'Mark Public'}
+                                        </button>
+                                        <button className="action-btn">View</button>
+                                        <button className="action-btn" onClick={() => handleEditClick(event)}>Edit</button>
+                                        <button className="action-btn delete-btn" onClick={() => deleteEvent(event.id)}>Delete</button>
+                                    </div>
+                                }
+                            </div>
+                        ))
+                    ) : (
+                        <div className="no-events">No events available</div>
+                    )}
+                </div>}
 
             {/* Pagination */}
             {/* <div className="pagination">
@@ -160,8 +160,8 @@ const EventCards = ({ events,usertype }) => {
                             value={selectedEvent.location}
                             onChange={handleEditChange}
                         />
-                        <button onClick={() => handleEditSubmit(selectedEvent.id)}>Save</button>
-                        <button onClick={closeEditPopup}>Cancel</button>
+                        <button onClick={() => handleEditSubmit(selectedEvent.id)} style={{ height: '40px', width: '120px', backgroundColor: 'green', borderRadius: '5px', marginRight: '10px', color: 'white' }}>Save</button>
+                        <button onClick={closeEditPopup} style={{ height: '40px', width: '120px', backgroundColor: 'red', borderRadius: '5px', marginRight: '10px', color: 'white' }}>Cancel</button>
                     </div>
                 </div>
             )}
